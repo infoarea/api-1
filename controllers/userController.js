2db8e437aaaaee6c0209fcf6c857e7f986350af1
saiful <niloysaif9@gmail.com>
2022-11-15 21:38:59 +0600

    api changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ const { readFileSync, writeFileSync} = require('fs');
 const getAllUser = ( req, res )=> {
 
     //Get users Data from json DB
-   const users = JSON.parse(readFileSync(path.join(__dirname, '../db/user.json')))
+   const users = JSON.parse(readFileSync(path.join(__dirname, '../db/users.json')))
 
    //show user Data
     res.json(users)
@@ -26,7 +26,7 @@ const getAllUser = ( req, res )=> {
  */
  const createUser = ( req, res )=> {
 
-    const users = JSON.parse(readFileSync(path.join(__dirname, '../db/user.json')))
+    const users = JSON.parse(readFileSync(path.join(__dirname, '../db/users.json')))
 
     const { name, skill } = req.body;
 
@@ -40,14 +40,17 @@ const getAllUser = ( req, res )=> {
     }else {
 
 
-        users.push(req.body);
+        users.push({
+            id : Math.floor(Math.random() *100000),
+            name : name,
+            skill : skill
+        });
 
-        writeFileSync(path.join(__dirname, '../db/user.json'), JSON.stringify(users));
-
+        writeFileSync(path.join(__dirname, '../db/users.json'), JSON.stringify(users));
 
         res.status(201).json({
-            message : "Sent to DB sucess!"
-        })
+            message : "Sent Data sucess!"
+        });
 
     }
    
