bb779149b06a31b586eeeab26a60433f70a20286
saiful <niloysaif9@gmail.com>
2022-11-15 12:46:49 +0600

    api project

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const colors = require('colors');
 
 //express init
 const app = express();
-
+ 
 //dotenv init
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -19,4 +19,4 @@ app.use(express.urlencoded({ extended : false }));
 
 app.listen(PORT, ()=> {
     console.log(`server is running ${PORT}`.bgGreen.black);
-});
+}); 
