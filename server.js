cf906e49c6dd70534c6223f71b49cb55ea65c77b
saiful <niloysaif9@gmail.com>
2022-11-15 12:40:07 +0600

    api create

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+
+//express init
+const app = express();
+
+//dotenv init
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+
+//Data manage
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+
+app.listen(PORT, ()=> {
+    console.log(`server is running ${PORT}`.bgGreen.black);
+});
